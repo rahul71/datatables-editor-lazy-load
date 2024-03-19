b213c3eaab00c1560c06769e582202906f5f8652
rahul <rahul71@gmail.com>
2024-03-19 11:47:13 -0400

    This jquery datatables editor lazy loads

---
+++ src/app/shared-loaders/modules/datatables.module.ts
@@ -0,0 +1,13 @@
+import 'datatables.net/js/dataTables.js';
+import 'datatables.net-bs5/js/dataTables.bootstrap5.js';
+import 'datatables.net-buttons/js/dataTables.buttons.js';
+import 'datatables.net-buttons/js/buttons.html5.js';
+import 'datatables.net-buttons/js/buttons.print.js';
+import 'datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.js';
+import 'datatables.net-select-bs5/js/select.bootstrap5.js';
+import 'datatables.net-rowreorder/js/dataTables.rowReorder.js';
+import 'datatables.net-rowreorder-bs5/js/rowReorder.bootstrap5.js';
+import 'datatables.net-fixedheader-bs5/js/fixedHeader.bootstrap5';
+
+// import 'datatables.net-editor/js/dataTables.editor.js';
+// import 'datatables.net-editor-bs5/js/editor.bootstrap5.js';
